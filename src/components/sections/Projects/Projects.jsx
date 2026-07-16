6873187248b6d413142b8cb6b8fcb49e7ec88573
Daniel-kripta <danielgz-profesional@outlook.es>
2026-07-17 00:44:42 +0100

    feat: stack tech list

--- src/components/sections/Projects/Projects.jsx
+++ src/components/sections/Projects/Projects.jsx
@@ -1,35 +1,51 @@
-import styles from "./Projects.module.css"
-import data from "../../../data/projects.js"
+import styles from "./Projects.module.css";
+import data from "../../../data/projects.js";
 
+export default function Projects({ type }) {
+  const projects = data[type];
+  function techList(items) {
+    return items.map((tech) =>
+      tech.img !== undefined ? (
+        <li key={tech.name}>
+          <img className={styles.techImg} src={tech.img} alt={tech.name} />
+        </li>
+      ) : (
+        <li key={tech.name}>{tech.name}</li>
+      ),
+    );
+  }
 
-export default function Projects({type}){
+  return (
+    <section id="proyectos" className={styles.projectSection}>
+      {projects.map((project) => (
+        <div key={project.id} className={styles.projectCard}>
+          <div>
+            <h3>{project.name}</h3>
+            <img
+              src={project.screenshot}
+              className={styles.screenshot}
+              alt={project.name}
+            />
+            <p>{project.description}</p>
+          </div>
+          <div className={styles.stackTech}>
+            <h4>Frameworks</h4>
+            <ul>
+              {techList(project.stack.filter((t) => t.type === "framework"))}
+            </ul>
 
-    const projects = data[type]
-    function techList(items) {
-    return items.map((tech) => (
-        <li key={tech.name}><img className={styles.techImg} src={tech.img} alt={tech.name} /></li>
-        ))
-    }
+            <h4>Lenguajes</h4>
+            <ul>
+              {techList(project.stack.filter((t) => t.type === "language"))}
+            </ul>
 
-
-    return(
-        <section id="proyectos" className={styles.projectSection}>
-            {projects.map((project) => (
-                <div key={project.id} className={styles.projectCard}>
-                    <div>
-                        <h3>{project.name}</h3>
-                        <img src={project.screenshot} className={styles.screenshot} alt={project.name} />
-                        <p>{project.description}</p>
-                    </div>
-                    <div className={styles.stackTech}>
-                        <h4>Frameworks</h4>
-                        <ul>{techList(project.stack.filter(t => t.type === "framework"))}</ul>
-
-                        <h4>Lenguajes</h4>
-                        <ul>{techList(project.stack.filter(t => t.type === "language"))}</ul>
-                    </div>
-                </div>
-            ))}
-        </section>
-    )
+            <h4>Recursos</h4>
+            <ul>
+              {techList(project.stack.filter((t) => t.type === "resource"))}
+            </ul>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
