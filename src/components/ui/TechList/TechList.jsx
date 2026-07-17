c926d2663099816000f6e8c89748b23532db51c2
Daniel-kripta <danielgz-profesional@outlook.es>
2026-07-17 15:55:29 +0100

    feat: techList module

--- src/components/ui/TechList/TechList.jsx
+++ src/components/ui/TechList/TechList.jsx
@@ -1,22 +1,22 @@
 import styles from "./TechList.module.css"
 
 export default function TechList({project, type}) {
-  return(
-  <>
-  <h4>{type}</h4>
-  <ul className={styles.techList}>
-    {project.stack
-      .filter((t) => t.type === type)
-      .map((tech) => (
-        <li key={tech.name}>
-          <img
-            className={styles.techImg}
-            src={tech.img}
-            alt={tech.name}
-          />
-        </li>
-      ))}
-  </ul>
-  </>
-  )
+  return (
+    <>
+      <h4 className={styles.techListTitle}>{type}</h4>
+      <ul className={styles.techList}>
+        {project.stack
+          .filter((t) => t.type === type)
+          .map((tech) => (
+            <li key={tech.name}>
+              {tech.img !== undefined
+              ? <img className={styles.techImg} src={tech.img} alt={tech.name} />
+              : tech.type === "resource"
+              ? <><span className={styles.subtype}>{tech.subtype}</span> {tech.name}</>
+              : tech.name}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
