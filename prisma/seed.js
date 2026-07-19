770dc35bb95dd813ae81994e26272dc57defbd23
Daniel-kripta <danielgz-profesional@outlook.es>
2026-07-19 18:21:09 +0100

    fix: seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,8 +1,10 @@
 import "dotenv/config"
 import pkg from "@prisma/client"
 const { PrismaClient } = pkg
+import { PrismaPg } from "@prisma/adapter-pg"
 
-const prisma = new PrismaClient()
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
     await prisma.stack.deleteMany()
