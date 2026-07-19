ed3c112ff3ab9d41bcb211ffaf5c5541271ba20f
Daniel-kripta <danielgz-profesional@outlook.es>
2026-07-19 19:01:26 +0100

    fix: iconlinks

--- src/components/ui/IconsLink/IconsLink.jsx
+++ src/components/ui/IconsLink/IconsLink.jsx
@@ -4,14 +4,14 @@ import styles from "./IconsLink.module.css"
 export default function IconsLink({project}) {
   return (
     <>
-      {project.url.find((u) => u.name === "GitHub") && 
-        <a title={project.url.find((u) => u.name === "GitHub").description} href={project.url.find((u) => u.name === "GitHub").url}>
+      {project.urls.find((u) => u.name === "GitHub") &&
+        <a title={project.urls.find((u) => u.name === "GitHub").description} href={project.urls.find((u) => u.name === "GitHub").url}>
           <IconGitHub className={styles.iconLinks} />
         </a>
       }
 
-      {project.url.find((u) => u.name === "Demo") && 
-        <a title={project.url.find((u) => u.name === "Demo").description} href={project.url.find((u) => u.name === "Demo").url}>
+      {project.urls.find((u) => u.name === "Demo") &&
+        <a title={project.urls.find((u) => u.name === "Demo").description} href={project.urls.find((u) => u.name === "Demo").url}>
           <IconDemoPage className={styles.iconLinks} />
         </a>
       }
